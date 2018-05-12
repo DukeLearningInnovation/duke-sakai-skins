64ae58a14df9cd4d1d7c6d81fa20bee30669a6f0
Michael Greene <profmikegreene@gmail.com>
2018-05-12 06:55:36 -0400

    minor bugs

--- devdeploy.sh
+++ devdeploy.sh
@@ -1,8 +1,8 @@
-rm -rf dev/dukedev.longsight.com/library/skin/duke-default/*
-cp -R target/duke-default/* dev/dukedev.longsight.com/library/skin/duke-default
-cd dev/dukedev.longsight.com/library/skin/duke-default
-mv access.css access.css%3fversion=ad
-mv portal.css portal.css%3fversion=ad
-mv print.css print.css%3fversion=ad
-mv tool.css tool.css%3fversion=ad
+rm -rf dev/sakai.duke.edu/library/skin/duke-default/*
+cp -R target/duke-default/* dev/sakai.duke.edu/library/skin/duke-default
+cd dev/sakai.duke.edu/library/skin/duke-default
+mv access.css access.css%3fversion=ae
+mv portal.css portal.css%3fversion=ae
+mv print.css print.css%3fversion=ae
+mv tool.css tool.css%3fversion=ae
 cd ~/dev/dukelearninginnovation/duke-sakai-skins
