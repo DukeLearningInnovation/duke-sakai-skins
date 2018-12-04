75f18bb184fcbd6c267cd8784df50a61f1843ac4
Michael Greene <profmikegreene@gmail.com>
2018-12-04 16:12:34 -0500

    fixes #36

--- src/duke-extend/js/src/de.js
+++ src/duke-extend/js/src/de.js
@@ -3,6 +3,43 @@
         .addClass('embed-responsive-item')
         .parent().addClass('embed-responsive-16by9 embed-responsive');
 
+
+        /*****************************
+        Look for inline multiple choice questions in Lessons
+        Part of the fix for #36
+        ******************************/ 
+
+        //Find all multiple choice questions on the page
+        var mcqs = $(".mainList .multipleChoiceForm");
+
+        //Iterate over the questions, if they have already
+        //been answered, add a disabled class to the form
+        mcqs.each(function(){
+            if( $(this).find('input[value="Submit Answer"]').prop('disabled')){
+                $(this).addClass('disabled');
+            }
+        });
+
+        //For questions that have not yet been answered
+        //this emulated edX answer hover behavior. The
+        //entire answer container is hover/clickable
+        //not just the radio button itself
+        $(".mainList .multipleChoiceForm").on("click", ".multipleChoiceAnswer",function(e){
+            var radio = $(this).find('input[type="radio"]');
+            var submitBtn = $( this ).parent().find('input[value="Submit Answer"]');
+
+            if(!submitBtn.prop('disabled')){
+                if(radio.prop( "checked" )){
+                    radio.prop("checked", false);
+
+                } else{
+                    radio.prop( "checked", true );
+                }
+                //override inline style put in by lessons
+                submitBtn.addClass('btn-primary').css('color','#fff !important');
+            }
+                
+        });
         /* Track outbound links in Google Analytics */
         /*
         //"use strict";
