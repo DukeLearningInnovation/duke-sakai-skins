ac53a1239471f3bf355e261305a5d35552f90b14
Michael Greene <profmikegreene@gmail.com>
2021-11-30 13:54:34 -0500

    duke-292 review most files from 20.x skins

--- library/dist/duke-divinity/js/morpheus.scripts.js
+++ library/dist/duke-divinity/js/morpheus.scripts.js
@@ -189,127 +189,6 @@ var dukeAddBodyClasses = function(){
 dukeAddBodyClasses();
 
 /////////////////////////////////////////////////
-// Adjust hamburger menu when system alerts are active
-////////////////////////////////////////////////
-
-// var dukeCollapseToolsAdjustedTop = 0;
-
-// // Call the observer to watch Mrphs-portalBody for
-// // dynamic loading of the pasystem
-// var dukeAdjustNodesForPASystem = function(nodes,top) {
-//     document.querySelectorAll(nodes).forEach(el => el.style.top = `${top}px`); 
-
-// };
-
-
-
-
-// var dukeWatchForPasystemLoad = function(nodesToMove){
-//     // Select the node that will be observed for mutations
-//     var targetNode = document.getElementsByClassName('Mrphs-portalBody')[0];
-
-//     // Options for the observer (which mutations to observe)
-//     var config = { childList: true };
-
-//     // Callback function to execute when mutations are observed
-//     var portalMutationCallback = function(mutationsList, portalObserver) {
-
-//         for(var mutation of mutationsList) {
-
-//             if (mutation.type == 'childList') {
-
-//                 var mutationTarget = mutation.target.childNodes[0];
-
-//                 if (mutationTarget.className == 'pasystem-banner-alerts'){
-                    
-//                     // Adjust hamburger for initial load of pasystem
-//                     dukeCollapseToolsAdjustedTop = document.getElementsByClassName('pasystem-banner-alerts')[0].clientHeight;
-//                     dukeAdjustNodesForPASystem(nodesToMove, dukeCollapseToolsAdjustedTop);
-//                     // document.querySelectorAll(nodesToMove).forEach(el => el.style.top = `${dukeCollapseToolsAdjustedTop}px`); 
-//                     // Start observing events that change the pasystem
-//                     dukeWatchForPasystemChange(nodesToMove);
-
-//                     // disconnect the portalBody observer, it is no longer needed
-//                     // once the pasystem is loaded into the DOM
-//                     portalObserver.disconnect();
-//                 }
-//             }
-//         }
-//     };
-
-//     // Create an observer instance linked to the callback function
-//     var portalObserver = new MutationObserver(portalMutationCallback);
-
-//     // Start observing the target node for configured mutations
-//     portalObserver.observe(targetNode, config);
-
-// };
-
-// var dukeWatchForPasystemChange = function(nodesToMove){
-
-//     // Select the node that will be observed for mutations
-//     var pasystemNode = document.getElementsByClassName('pasystem-banner-alerts')[0];
-//     var pasystemToggleNode = document.getElementsByClassName('pasystem-banner-alert-toggle')[0];
-
-//     // Options for the observer (which mutations to observe)
-//     var config = {
-//         childList: true,
-//         attributes: true,
-//         subtree:true,
-//         attributeOldValue: true
-//     };
-
-//     // Callback function to execute when mutations are observed
-//     var pasystemMutationCallback = function(mutationsList, pasystemObserver) {
-
-//         for(var mutation of mutationsList) {
-
-//             if (mutation.type == 'childList') {
-//                     dukeCollapseToolsAdjustedTop = pasystemNode.clientHeight;
-//                     // $PBJQ('#toolsNav-toggle-li').css({'top': dukeCollapseToolsAdjustedTop});
-//                     // $PBJQ('.Mrphs-siteHierarchy .Mrphs-hierarchy--siteName').css({'top': dukeCollapseToolsAdjustedTop});
-//                     dukeAdjustNodesForPASystem(nodesToMove, dukeCollapseToolsAdjustedTop);
-//             }
-            
-//             if (mutation.type == 'attributes' && mutation.target.className.includes('pasystem-banner-alert')) {
-//                     dukeCollapseToolsAdjustedTop = pasystemNode.clientHeight;
-//                     // $PBJQ('#toolsNav-toggle-li').css({'top': dukeCollapseToolsAdjustedTop});
-//                     // $PBJQ('.Mrphs-siteHierarchy .Mrphs-hierarchy--siteName').css({'top': dukeCollapseToolsAdjustedTop});
-//                     dukeAdjustNodesForPASystem(nodesToMove, dukeCollapseToolsAdjustedTop);
-//             }
-//         }
-//     };
-
-//     // Create an observer instance linked to the callback function
-//     var pasystemObserver = new MutationObserver(pasystemMutationCallback);
-//     var pasystemToggleObserver = new MutationObserver(pasystemMutationCallback);
-    
-//     // Start observing the target node for configured mutations
-//     pasystemObserver.observe(pasystemNode, config);
-    
-//     // The toggleNode is in a separate part of the DOM
-//     // so it needs its own observer
-//     pasystemToggleObserver.observe(pasystemToggleNode, config);
-
-// };
-
-
-
-// //Adjust the toolCollapse on window scroll
-// var dukeWatchForPASystemOnScroll = function(nodes){
-//     window.addEventListener('scroll', event => {
-//         if (window.scrollY > 0) {
-//             dukeAdjustNodesForPASystem(nodes, 0);
-//         } else {
-//             dukeAdjustNodesForPASystem(nodes, dukeCollapseToolsAdjustedTop);
-//         }
-//     });    
-// };
-
-// dukeWatchForPasystemLoad("#toolsNav-toggle-li");
-// dukeWatchForPASystemOnScroll("#toolsNav-toggle-li");
-
-/////////////////////////////////////////////////
 // Make all MOTD cards the same height
 ////////////////////////////////////////////////
 if (document.title === 'Sakai : Home : Overview') {
@@ -336,7 +215,9 @@ if ( document.getElementById('Mrphs-footer--details__info') !== 'null'){
     document.getElementById('serverTime').insertAdjacentHTML('afterend',buildTimeHtml);
 }
 
-// #222
+///////////////////////////////////////////////////
+// #222 Make logo take you to the home page
+///////////////////////////////////////////////////
 let logoContainer = document.querySelector('.Mrphs-headerLogo--institution');
 let homeLinkContainer = document.querySelector('.Mrphs-sitesNav__menuitem--myworkspace .link-container');
 logoContainer.style.cursor = 'pointer';
