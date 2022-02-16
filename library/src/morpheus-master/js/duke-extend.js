27ac0b435816b92ff034b09357044fcb8a000cd5
Michael Greene <profmikegreene@gmail.com>
2022-02-16 16:24:24 -0500

    Run prettier formatting on duke source code

--- library/src/morpheus-master/js/duke-extend.js
+++ library/src/morpheus-master/js/duke-extend.js
@@ -1,7 +1,8 @@
 // console.log('duke.extend.js');
 
-if (typeof portal.siteTitle !== 'undefined'){
-    let dukeSiteTitle = `<span class="Duke-headerlogo--siteTitle">${portal.siteTitle}</span>`;
-    document.getElementById('mastLogin').insertAdjacentHTML('beforebegin',dukeSiteTitle);
-
+if (typeof portal.siteTitle !== "undefined") {
+  let dukeSiteTitle = `<span class="Duke-headerlogo--siteTitle">${portal.siteTitle}</span>`;
+  document
+    .getElementById("mastLogin")
+    .insertAdjacentHTML("beforebegin", dukeSiteTitle);
 }
